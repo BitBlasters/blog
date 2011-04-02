a6da306c1cb668a3a346ba2b60d4c7a1c5c9130b
BitBlasters <scottmitchell@mac.com>
2011-04-02 13:53:54 -0600

    Added more code

--- Gemfile
+++ Gemfile
@@ -29,3 +29,12 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 # group :development, :test do
 #   gem 'webrat'
 # end
+
+group :development do
+	gem 'rspec-rails', '2.0.1'
+end
+
+group :test do
+	gem 'rspec', '2.0.1'
+	gem 'spork', '0.8.4'
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,17 @@
 class Article < ActiveRecord::Base
   validates :title, :presence => true
   validates :body, :presence => true
+  
+  belongs_to :user
+  has_and_belongs_to_many :categories
+  has_many :comments
+  
+  scope :published, where("articles.published_at is NOT NULL")
+  scope :draft, where("articles.publised_at is NULL")
+  scope :recent, lambda{ published.where("artcles.published_at > ?", 1.week.ago.to_date) }
+  scope :where_title, lambda{ |term| where("articles.title LIKE ?", "%#{term}%")}
+  
+  def long_title
+    "#{title} - #{published_at}"
+  end
 end
